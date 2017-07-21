5eefa732f1713416289368a4bb847c94ade2f958
unknown <mltseng@uci.edu>
2017-07-20 20:10:09 -0700

    Added Extension.kt

--- FateGR/app/src/main/java/com/derek/fategr/MainActivity.kt
+++ FateGR/app/src/main/java/com/derek/fategr/MainActivity.kt
@@ -3,6 +3,8 @@ package com.derek.fategr
 import android.os.Bundle
 import android.support.design.widget.FloatingActionButton
 import android.support.design.widget.Snackbar
+import android.support.v4.app.Fragment
+import android.support.v4.app.FragmentManager
 import android.support.v7.app.AppCompatActivity
 import android.support.v7.widget.Toolbar
 import android.view.View
@@ -17,30 +19,38 @@ class MainActivity : AppCompatActivity() {
         val toolbar = findViewById(R.id.toolbar) as Toolbar
         setSupportActionBar(toolbar)
 
-        val fab = findViewById(R.id.fab) as FloatingActionButton
-        fab.setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                    .setAction("Action", null).show()
+        if(savedInstanceState == null){
+            changeFragment(NewsFragment())
         }
     }
 
-    override fun onCreateOptionsMenu(menu: Menu): Boolean {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        menuInflater.inflate(R.menu.menu_main, menu)
-        return true
+
+    fun changeFragment(f: Fragment, cleanStack: Boolean = false){
+        val ft = supportFragmentManager.beginTransaction()
+        if(cleanStack){
+            clearBackStack()
+        }
+        ft.setCustomAnimations(R.anim.abc_fade_in, R.anim.abc_fade_out, R.anim.abc_popup_enter, R.anim.abc_popup_exit)
+        ft.replace(R.id.activity_base_content, f)
+        ft.addToBackStack(null)
+        ft.commit()
     }
 
-    override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        val id = item.itemId
-
-
-        if (id == R.id.action_settings) {
-            return true
+    fun clearBackStack(){
+        val manager = supportFragmentManager;
+        if(manager.backStackEntryCount > 0){
+            val first = manager.getBackStackEntryAt(0)
+            manager.popBackStack(first.id, FragmentManager.POP_BACK_STACK_INCLUSIVE)
         }
+    }
 
-        return super.onOptionsItemSelected(item)
+    override fun onBackPressed() {
+        val fragmentManager = supportFragmentManager
+        if(fragmentManager.backStackEntryCount > 1){
+            fragmentManager.popBackStack()
+        }
+        else{
+            finish()
+        }
     }
 }
---
+++ FateGR/app/src/main/java/commons/Extensions.kt
@@ -0,0 +1,18 @@
+@file:JvmName("ExtensionUtils")
+
+package commons
+
+import android.view.View
+import android.view.ViewGroup
+
+/**
+ * Created by Michael on 7/20/2017.
+ */
+
+//This adds a new method to ViewGroup, but not modifying the ViewGroup class
+//Internally, it is a static method
+//it's like a regular class method, accesses instance of the class with "this' and access to local variable "context"
+//You can define default values in parameters with Kotlin
+fun ViewGroup.inflate(layoutId: Int, attachToRoot: Boolean = false): View {
+    return android.view.LayoutInflater.from(context).inflate(layoutId, this, attachToRoot)
+}
