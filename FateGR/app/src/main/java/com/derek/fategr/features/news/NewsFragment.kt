a47135197a2150bd36d020e206afa0eeb0742dbd
unknown <mltseng@uci.edu>
2017-07-24 21:59:39 -0700

    Created working sample, added internet permissions to show images'

--- FateGR/app/src/main/java/com/derek/fategr/features/news/NewsFragment.kt
+++ FateGR/app/src/main/java/com/derek/fategr/features/news/NewsFragment.kt
@@ -6,6 +6,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.derek.fategr.R
+import com.derek.fategr.commons.RedditNewsItem
 import com.derek.fategr.commons.extensions.inflate
 import com.derek.fategr.features.news.adapter.NewsAdapter
 import kotlinx.android.synthetic.main.news_fragment.*
@@ -26,6 +27,24 @@ class NewsFragment : Fragment(){
         news_list.layoutManager = LinearLayoutManager(context)
 
         initAdapter()
+
+        if(savedInstanceState == null){
+            //a mutable list can be modified
+            val news = mutableListOf<RedditNewsItem>()
+            //this returns an IntRange; it extends from IntProgression and this implements Iterable
+            //decremental 10 downTo 1
+            for(i in 1..10){
+                news.add(RedditNewsItem(
+                        "author$i",
+                        "Title $i",
+                        i, //number of comments
+                        1457207701L - (i * 200),
+                        "https://i.imgur.com/tGbaZCY.jpg", //image url
+                        "url"
+                ))
+            }
+            (news_list.adapter as NewsAdapter).addNews(news)
+        }
     }
 
     private fun initAdapter(){
--- FateGR/app/src/main/java/com/derek/fategr/features/news/adapter/NewsAdapter.kt
+++ FateGR/app/src/main/java/com/derek/fategr/features/news/adapter/NewsAdapter.kt
@@ -3,6 +3,7 @@ package com.derek.fategr.features.news.adapter
 import android.support.v4.util.SparseArrayCompat
 import android.support.v7.widget.RecyclerView
 import android.view.ViewGroup
+import com.derek.fategr.commons.RedditNewsItem
 import com.derek.fategr.commons.adapter.AdapterConstants
 import com.derek.fategr.commons.adapter.ViewType
 import com.derek.fategr.commons.adapter.ViewTypeDelegateAdapter
@@ -40,5 +41,42 @@ class NewsAdapter: RecyclerView.Adapter<RecyclerView.ViewHolder>(){
 
     override fun getItemViewType(position: Int): Int{
         return this.items.get(position).getViewType()
+    }
+
+    fun addNews(news: List<RedditNewsItem>){
+        //first remove loading and notify
+        val initPosition = items.size - 1
+        items.removeAt(initPosition)
+        notifyItemRemoved(initPosition)
+
+        //insert news and the loading at the end of the list
+        items.addAll(news)
+        items.add(loadingItem)
+        notifyItemRangeChanged(initPosition, items.size + 1/*plus loading item */)
+    }
+
+    fun clearAndAddNews(news: List<RedditNewsItem>){
+        items.clear()
+        notifyItemRangeRemoved(0, getLastPosition())
+
+        items.addAll(news)
+        items.add(loadingItem)
+        notifyItemRangeInserted(0, items.size)
+    }
+
+    fun getNews(): List<RedditNewsItem>{
+        //filters and transforms every item from a list to another one
+        //filter allows us to iterate a list and filter items that don't apply certain conditions
+        //returns only news items
+        //map transforms the items from a list
+        //we cast a ViewType object to a RedditNewsItem
+        //Lambda function, a function that is not delcared but passed immediately as an expression
+        return items.filter{it.getViewType() == AdapterConstants.NEWS}.map{it as RedditNewsItem}
+    }
+
+    private fun getLastPosition() = if(items.lastIndex == -1){
+        0
+    } else{
+        items.lastIndex
     }
 }
