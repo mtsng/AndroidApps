550c7f4dd950f978dc935eb25717c32145277e38
unknown <mltseng@uci.edu>
2017-08-03 11:13:22 -0700

    Added Webview to show full post

--- Fate_GR/app/src/main/java/com/derek/fate_gr/adapter/PostClickedAdapter.java
+++ Fate_GR/app/src/main/java/com/derek/fate_gr/adapter/PostClickedAdapter.java
@@ -3,6 +3,7 @@ package com.derek.fate_gr.adapter;
 import android.content.Context;
 import android.content.Intent;
 import android.graphics.Bitmap;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
@@ -12,6 +13,7 @@ import android.widget.TextView;
 
 import com.derek.fate_gr.CommentsActivity;
 import com.derek.fate_gr.R;
+import com.derek.fate_gr.WebViewActivity;
 import com.nostra13.universalimageloader.cache.memory.impl.WeakMemoryCache;
 import com.nostra13.universalimageloader.core.DisplayImageOptions;
 import com.nostra13.universalimageloader.core.ImageLoader;
@@ -22,6 +24,8 @@ import com.nostra13.universalimageloader.core.display.FadeInBitmapDisplayer;
 import com.nostra13.universalimageloader.core.listener.ImageLoadingListener;
 
 import org.w3c.dom.Text;
+
+import static android.content.ContentValues.TAG;
 
 /**
  * Created by Michael on 7/31/2017.
@@ -46,6 +50,8 @@ public class PostClickedAdapter {
     private Intent mIntent;
     private ListView mCommentView;
     private ProgressBar mProgressBar;
+    private String permalink;
+    private String BASE_URL = "https://www.reddit.com";
 
     public PostClickedAdapter(Context context, TextView title,
                               TextView author, TextView selftext,
@@ -69,12 +75,23 @@ public class PostClickedAdapter {
         postImageURL = mIntent.getStringExtra("@string/post_image");
         postSelftext = mIntent.getStringExtra("@string/post_selftext");
         postFlair = mIntent.getStringExtra("@string/post_flair");
+        permalink = mIntent.getStringExtra("@string/post_permalink");
 
         mTitle.setText(postTitle);
         mAuthor.setText(postAuthor);
         mFlair.setText(postFlair);
         mSelftext.setText(postSelftext);
         displayImage(postImageURL, mImage, mProgressBar);
+
+        mImage.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                Log.d(TAG, "onClick: Opening URL in webview: ");
+                Intent intent = new Intent(mContext, WebViewActivity.class);
+                intent.putExtra("url", BASE_URL + permalink);
+                mContext.startActivity(intent);
+            }
+        });
     }
 
     private void displayImage(String imgUrl, ImageView imgView, final ProgressBar progressBar){
